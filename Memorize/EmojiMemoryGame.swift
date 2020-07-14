54528a500e88a2204e3ec979a572c45fd97262e5
Max Pereira <me@maxpereira.com>
2020-07-13 21:59:13 -0400

    first commit

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,41 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Max Pereira on 7/9/20.
+//  Copyright © 2020 Max Pereira. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻","🎃","🕷","🕸","🍫"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+                return emojis[pairIndex]
+        }
+    }
+
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        objectWillChange.send()
+        model.choose(card: card)
+    }
+}
+
+struct EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
